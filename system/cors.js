650b56f3c74bf19485b420255a868d622de0b546
MatthewJRoybal <matthewjroybal@gmail.com>
2017-08-17 22:18:54 -0700

    Updating

--- system/cors.js
+++ system/cors.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // allows any domain is allowed to talk to this domain --> need to say only this server to this client
 router.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'https://quizard-me.herokuapp.com/');
+    res.header('Access-Control-Allow-Origin', 'http://www.quizard.me');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
     res.header('Access-Control-Expose-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
